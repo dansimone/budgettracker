df01e6ec7f9051a18abd235a7811592d45a92bdd
dan.simone@oracle.com <dan.simone@oracle.com>
2016-04-20 18:01:24 -0700

    Add capability to remove categories.

--- budgettracker.js
+++ budgettracker.js
@@ -74,12 +74,12 @@ if (Meteor.isClient) {
         Session.set('txnSortField', {date: -1});
       }
       return Transactions.find(
-          {
-            date: {
-              $gte: firstDay,
-              $lt: lastDay
-            }
-          }, {sort: Session.get('txnSortField')});
+        {
+          date: {
+            $gte: firstDay,
+            $lt: lastDay
+          }
+        }, {sort: Session.get('txnSortField')});
     },
     getAmount: function () {
       return Math.abs(this.amount).toFixed(2);
@@ -116,140 +116,140 @@ if (Meteor.isClient) {
     }
   });
   Template.transactionslist.events(
-      {
-        'click div.add-transaction a': function () {
-          event.preventDefault();
-          if (Session.get("adding_transaction")) {
-            Session.set("adding_transaction", false);
-          }
-          else {
-            Session.set("adding_transaction", true);
-          }
-        },
-        'submit form.add-transaction': function () {
-          // Prevent default browser form submit
-          event.preventDefault();
-
-          var error = false;
-          var amount = event.target.amount.value;
-          if (amount.length == 0) {
-            Session.set("formError-transaction-amount", true);
-            error = true;
-          }
-          else {
-            Session.set("formError-transaction-amount", false);
-          }
-          var dateString = event.target.date.value + " PDT";
-          if (event.target.date.value.length == 0) {
-            Session.set("formError-transaction-date", true);
-            error = true;
-          }
-          else {
-            Session.set("formError-transaction-date", false);
-          }
-          // Return control if any form errors found
-          if (error) {
-            return;
-          }
-
-          var comments = event.target.comments.value;
-          var type = event.target.type.value;
-          var category;
-          if (type == "deposit") {
-            category = "deposit";
-          }
-          else {
-            category = event.target.category.value;
-          }
-
-          // If the date entered is today, use the exact time now as the Date, otherwise
-          // just use the day
-          var date = new Date(dateString);
-          var today = new Date();
-          if (today.getFullYear() == date.getFullYear() && today.getMonth() == date.getMonth()
-              && today.getDate() == date.getDate()) {
-            date = today;
-          }
-
-          Transactions.insert({
-            category_id: category,
-            amount: parseFloat(-amount),
-            date: date,
-            type: type,
-            comments: comments
-          });
+    {
+      'click div.add-transaction a': function () {
+        event.preventDefault();
+        if (Session.get("adding_transaction")) {
           Session.set("adding_transaction", false);
-        },
-        'click select.transaction-type': function () {
-          var name = event.target.value;
-          if (name == "deposit") {
-            Session.set("adding_transaction_withdrawal", false);
-          } else {
-            Session.set("adding_transaction_withdrawal", true);
-          }
-        },
-        'click th.sort-categories': function () {
-          var sortOrder = 1;
-          if (Session.get('txnSortField') != null && Session.get('txnSortField').category_id != null) {
-            sortOrder = Session.get('txnSortField').category_id * -1;
-          }
-          Session.set('txnSortField', {category_id: sortOrder, date: -1});
-        },
-        'click th.sort-dates': function () {
-          var sortOrder = 1;
-          if (Session.get('txnSortField') != null && Session.get('txnSortField').date != null) {
-            sortOrder = Session.get('txnSortField').date * -1;
-          }
-          Session.set('txnSortField', {date: sortOrder});
-        },
-        'click th.sort-amounts': function () {
-          var sortOrder = 1;
-          if (Session.get('txnSortField') != null && Session.get('txnSortField').amount != null) {
-            sortOrder = Session.get('txnSortField').amount * -1;
-          }
-          Session.set('txnSortField', {amount: sortOrder, date: -1});
-        },
-        'click .form-group.has-error input,textarea': function (event) {
-          Session.set("formError-" + event.target.closest('.form-group').getAttribute("name"), false);
-        },
-        'click tr.transaction-row td span.transaction-amount': function () {
-          var cell = $(event.target);
-          var currentAmount = cell.text();
-          cell.text("");
-          var txnId = cell.closest('tr').attr("id");
-          $('<input />').appendTo(cell).val(currentAmount).select().on("blur keyup",
-              function (event) {
-                // Ignore anything that is a key press but *not* an enter
-                if (event.keyCode != null && event.keyCode != 13) {
-                  return;
-                }
-                // Restore value if not a number
-                var newAmount;
-                if (isNaN($(this).val())) {
-                  newAmount = currentAmount;
-                }
-                else {
-                  newAmount = parseFloat($(this).val()).toFixed(2);
-                }
-                cell.find("input").remove();
-                if (newAmount != currentAmount) {
-                  cell.text("");
-                  Transactions.update({_id: txnId}, {$set: {amount: parseFloat(newAmount)}});
-                }
-                else {
-                  cell.text(newAmount);
-                }
-              });
-        },
-        'click tr.transaction-row td a.transaction-remove': function () {
-          event.preventDefault();
-          var cell = $(event.target);
-          var txnId = cell.closest('tr').attr("id");
-          if (confirm("Are you sure you want to delete this transaction?") == true) {
-            Transactions.remove(txnId);
-          }
+        }
+        else {
+          Session.set("adding_transaction", true);
+        }
+      },
+      'submit form.add-transaction': function () {
+        // Prevent default browser form submit
+        event.preventDefault();
+
+        var error = false;
+        var amount = event.target.amount.value;
+        if (amount.length == 0) {
+          Session.set("formError-transaction-amount", true);
+          error = true;
+        }
+        else {
+          Session.set("formError-transaction-amount", false);
+        }
+        var dateString = event.target.date.value + " PDT";
+        if (event.target.date.value.length == 0) {
+          Session.set("formError-transaction-date", true);
+          error = true;
+        }
+        else {
+          Session.set("formError-transaction-date", false);
+        }
+        // Return control if any form errors found
+        if (error) {
+          return;
+        }
+
+        var comments = event.target.comments.value;
+        var type = event.target.type.value;
+        var category;
+        if (type == "deposit") {
+          category = "deposit";
+        }
+        else {
+          category = event.target.category.value;
+        }
+
+        // If the date entered is today, use the exact time now as the Date, otherwise
+        // just use the day
+        var date = new Date(dateString);
+        var today = new Date();
+        if (today.getFullYear() == date.getFullYear() && today.getMonth() == date.getMonth()
+          && today.getDate() == date.getDate()) {
+          date = today;
+        }
+
+        Transactions.insert({
+          category_id: category,
+          amount: parseFloat(-amount),
+          date: date,
+          type: type,
+          comments: comments
+        });
+        Session.set("adding_transaction", false);
+      },
+      'click select.transaction-type': function () {
+        var name = event.target.value;
+        if (name == "deposit") {
+          Session.set("adding_transaction_withdrawal", false);
+        } else {
+          Session.set("adding_transaction_withdrawal", true);
+        }
+      },
+      'click th.sort-categories': function () {
+        var sortOrder = 1;
+        if (Session.get('txnSortField') != null && Session.get('txnSortField').category_id != null) {
+          sortOrder = Session.get('txnSortField').category_id * -1;
+        }
+        Session.set('txnSortField', {category_id: sortOrder, date: -1});
+      },
+      'click th.sort-dates': function () {
+        var sortOrder = 1;
+        if (Session.get('txnSortField') != null && Session.get('txnSortField').date != null) {
+          sortOrder = Session.get('txnSortField').date * -1;
+        }
+        Session.set('txnSortField', {date: sortOrder});
+      },
+      'click th.sort-amounts': function () {
+        var sortOrder = 1;
+        if (Session.get('txnSortField') != null && Session.get('txnSortField').amount != null) {
+          sortOrder = Session.get('txnSortField').amount * -1;
+        }
+        Session.set('txnSortField', {amount: sortOrder, date: -1});
+      },
+      'click .form-group.has-error input,textarea': function (event) {
+        Session.set("formError-" + event.target.closest('.form-group').getAttribute("name"), false);
+      },
+      'click tr.transaction-row td span.transaction-amount': function () {
+        var cell = $(event.target);
+        var currentAmount = cell.text();
+        cell.text("");
+        var txnId = cell.closest('tr').attr("id");
+        $('<input />').appendTo(cell).val(currentAmount).select().on("blur keyup",
+          function (event) {
+            // Ignore anything that is a key press but *not* an enter
+            if (event.keyCode != null && event.keyCode != 13) {
+              return;
+            }
+            // Restore value if not a number
+            var newAmount;
+            if (isNaN($(this).val())) {
+              newAmount = currentAmount;
+            }
+            else {
+              newAmount = parseFloat($(this).val()).toFixed(2);
+            }
+            cell.find("input").remove();
+            if (newAmount != currentAmount) {
+              cell.text("");
+              Transactions.update({_id: txnId}, {$set: {amount: parseFloat(newAmount)}});
+            }
+            else {
+              cell.text(newAmount);
+            }
+          });
+      },
+      'click tr.transaction-row td a.transaction-remove': function () {
+        event.preventDefault();
+        var cell = $(event.target);
+        var txnId = cell.closest('tr').attr("id");
+        if (confirm("Are you sure you want to delete this transaction?") == true) {
+          Transactions.remove(txnId);
         }
       }
+    }
   );
 
   /**
@@ -294,78 +294,94 @@ if (Meteor.isClient) {
     }
   });
   Template.categorieslist.events(
-      {
-        'click div.add-category a': function () {
-          event.preventDefault();
-          if (Session.get("adding_category")) {
-            Session.set("adding_category", false);
-          }
-          else {
-            Session.set("adding_category", true);
-          }
-        },
-        'submit form.add-category': function () {
-          // Prevent default browser form submit
-          event.preventDefault();
-          var error = false;
-          var name = event.target.name.value;
-          if (name.length == 0) {
-            Session.set("formError-category-name", true);
-            error = true;
-          }
-          else {
-            Session.set("formError-category-name", false);
-          }
-          var amount = event.target.amount.value;
-          if (amount.length == 0) {
-            Session.set("formError-category-amount", true);
-            error = true;
-          }
-          else {
-            Session.set("formError-category-amount", false);
-          }
-          // Return control if any form errors found
-          if (error) {
-            return;
-          }
-
-          Categories.insert({_id: name, amount: parseFloat(amount)});
+    {
+      'click div.add-category a': function () {
+        event.preventDefault();
+        if (Session.get("adding_category")) {
           Session.set("adding_category", false);
-        },
-        'click .form-group.has-error input,textarea': function (event) {
-          Session.set("formError-" + event.target.closest('.form-group').getAttribute("name"), false);
-        },
-        'click tr.category-row td span.category-amount': function () {
-          var cell = $(event.target);
-          var currentAmount = cell.text();
-          cell.text("");
-          var categoryId = cell.closest('tr').attr("id");
-          $('<input />').appendTo(cell).val(currentAmount).select().on("blur keyup",
-              function (event) {
-                // Ignore anything that is a key press but *not* an enter
-                if (event.keyCode != null && event.keyCode != 13) {
-                  return;
-                }
-                // Restore value if not a number
-                var newAmount;
-                if (isNaN($(this).val())) {
-                  newAmount = currentAmount;
-                }
-                else {
-                  newAmount = parseFloat($(this).val()).toFixed(2);
-                }
-                cell.find("input").remove();
-                if (newAmount != currentAmount) {
-                  cell.text("");
-                  Categories.update({_id: categoryId}, {$set: {amount: parseFloat(newAmount)}});
-                  console.log("Updated " + categoryId + " to " + newAmount);
-                }
-                else {
-                  cell.text(newAmount);
-                }
-              });
+        }
+        else {
+          Session.set("adding_category", true);
+        }
+      },
+      'submit form.add-category': function () {
+        // Prevent default browser form submit
+        event.preventDefault();
+        var error = false;
+        var name = event.target.name.value;
+        if (name.length == 0) {
+          Session.set("formError-category-name", true);
+          error = true;
+        }
+        else {
+          Session.set("formError-category-name", false);
+        }
+        var amount = event.target.amount.value;
+        if (amount.length == 0) {
+          Session.set("formError-category-amount", true);
+          error = true;
+        }
+        else {
+          Session.set("formError-category-amount", false);
+        }
+        // Return control if any form errors found
+        if (error) {
+          return;
+        }
+
+        Categories.insert({_id: name, amount: parseFloat(amount)});
+        Session.set("adding_category", false);
+      },
+      'click .form-group.has-error input,textarea': function (event) {
+        Session.set("formError-" + event.target.closest('.form-group').getAttribute("name"), false);
+      },
+      'click tr.category-row td span.category-amount': function () {
+        var cell = $(event.target);
+        var currentAmount = cell.text();
+        cell.text("");
+        var categoryId = cell.closest('tr').attr("id");
+        $('<input />').appendTo(cell).val(currentAmount).select().on("blur keyup",
+          function (event) {
+            // Ignore anything that is a key press but *not* an enter
+            if (event.keyCode != null && event.keyCode != 13) {
+              return;
+            }
+            // Restore value if not a number
+            var newAmount;
+            if (isNaN($(this).val())) {
+              newAmount = currentAmount;
+            }
+            else {
+              newAmount = parseFloat($(this).val()).toFixed(2);
+            }
+            cell.find("input").remove();
+            if (newAmount != currentAmount) {
+              cell.text("");
+              Categories.update({_id: categoryId}, {$set: {amount: parseFloat(newAmount)}});
+              console.log("Updated " + categoryId + " to " + newAmount);
+            }
+            else {
+              cell.text(newAmount);
+            }
+          });
+      },
+      'click tr.category-row td a.category-remove': function () {
+        event.preventDefault();
+        var cell = $(event.target);
+        var categoryId = cell.closest('tr').attr("id");
+        if (confirm("Are you sure you want to delete this category?") == true) {
+          Categories.remove(categoryId);
+          // Delete any matching transactions
+          matchingTransactions = Transactions.find(
+            {
+              category_id: categoryId
+            });
+          matchingTransactions.forEach(function (transaction) {
+            Transactions.remove(transaction._id);
+          });
         }
       }
+    }
   );
 }
 
@@ -377,13 +393,13 @@ if (Meteor.isServer) {
       _.each(transactions, function (transaction) {
         //console.log("OKOK " + transaction.amount);
         Transactions.insert(
-            {
-              category_id: transaction.category_id,
-              comments: transaction.comments,
-              date: new Date(transaction.date),
-              type: transaction.type.toLowerCase(),
-              amount: transaction.amount
-            }
+          {
+            category_id: transaction.category_id,
+            comments: transaction.comments,
+            date: new Date(transaction.date),
+            type: transaction.type.toLowerCase(),
+            amount: transaction.amount
+          }
         );
       })
     }
@@ -423,13 +439,13 @@ function getAmountUsedForMonth(transaction, monthStartDate) {
   var firstDay = new Date(monthStartDate.getFullYear(), monthStartDate.getMonth(), 1);
   var lastDay = new Date(monthStartDate.getFullYear(), monthStartDate.getMonth() + 1, 1);
   txnsThisMonth = Transactions.find(
-      {
-        category_id: transaction._id,
-        date: {
-          $gte: firstDay,
-          $lt: lastDay
-        }
+    {
+      category_id: transaction._id,
+      date: {
+        $gte: firstDay,
+        $lt: lastDay
       }
+    }
   );
   txnsThisMonth.forEach(function (txn) {
     amountUsed -= txn.amount;
